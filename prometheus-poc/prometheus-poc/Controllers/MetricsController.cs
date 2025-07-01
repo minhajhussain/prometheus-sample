120c61b84e99e39d4993f253646c55c09b021c69
Minhaj <minhajhussain899@gmail.com>
2025-07-01 18:15:48 +0500

    Initial Commit

---
+++ prometheus-poc/prometheus-poc/Controllers/MetricsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace prometheus_poc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetricsController : ControllerBase
+    {
+    }
+}
